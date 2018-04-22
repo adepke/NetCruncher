7f1daba91b7c7f7a57de287e4eb6220a0001d9a5
XenonicDev <andrewdepke@gmail.com>
2018-04-22 15:41:23 -0600

    Added StreamToPackage()

--- NetCruncher/Package.h
+++ NetCruncher/Package.h
@@ -13,24 +13,44 @@
 
 class Package
 {
+protected:
+	PackageHeader Header;
+
 public:
 	Package() {}
 	~Package()
 	{
-		for (int Iter = 0; Iter < Packs.size(); ++Iter)
+		for (size_t Iter = 0; Iter < Packs.size(); ++Iter)
 		{
 			delete Packs[Iter];
 		}
 	}
 
-	PackageHeader Header;
+	PackageHeader GetHeaderCopy()
+	{
+		Header.PackCount = Packs.size();
+
+		return Header;
+	}
+
+	void AddFlag(unsigned char Flag)
+	{
+		Header.Flags |= Flag;
+	}
+
+	void SetFlags(unsigned char Flags)
+	{
+		Header.Flags = Flags;
+	}
 
 	std::vector<Pack*> Packs;
+
+	friend Package StreamToPackage(std::vector<unsigned char>* Input);
 };
 
-std::vector<unsigned char>* PackageToStream(const Package& Input)
+std::vector<unsigned char>* PackageToStream(Package& Input)
 {
-	unsigned short RawHeader = PackageHeaderToBytes(Input.Header);
+	unsigned short RawHeader = PackageHeaderToBytes(Input.GetHeaderCopy());
 	auto* RawHeaderIter = (char*)&RawHeader;
 
 	auto* Stream = new std::vector<unsigned char>();
@@ -149,7 +169,177 @@ Package StreamToPackage(std::vector<unsigned char>* Input)
 
 	Output.Header = BytesToPackageHeader(RawHeader);
 
+	int PackOffset = 2 + Output.Header.PackCount;
 
+	// Read the Packs.
+	for (unsigned int Iter = 0; Iter < Output.Header.PackCount; ++Iter)
+	{
+		// Get the Type.
+		const PackType Type = static_cast<PackType>(Input->at(Iter + 2));
+
+		switch (Type)
+		{
+		case PackType::PT_Int8:
+		{
+			auto* _Pack = new IntPack8();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Int16:
+		{
+			auto* _Pack = new IntPack16();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Int32:
+		{
+			auto* _Pack = new IntPack32();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+			_Pack->Data[2] = Input->at(2 + PackOffset);
+			_Pack->Data[3] = Input->at(3 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Int64:
+		{
+			auto* _Pack = new IntPack64();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+			_Pack->Data[2] = Input->at(2 + PackOffset);
+			_Pack->Data[3] = Input->at(3 + PackOffset);
+			_Pack->Data[4] = Input->at(4 + PackOffset);
+			_Pack->Data[5] = Input->at(5 + PackOffset);
+			_Pack->Data[6] = Input->at(6 + PackOffset);
+			_Pack->Data[7] = Input->at(7 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Float8:
+		{
+			auto* _Pack = new FloatPack8();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Float16:
+		{
+			auto* _Pack = new FloatPack16();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Float24:
+		{
+			auto* _Pack = new FloatPack24();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+			_Pack->Data[2] = Input->at(2 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Float32:
+		{
+			auto* _Pack = new FloatPack32();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+			_Pack->Data[2] = Input->at(2 + PackOffset);
+			_Pack->Data[3] = Input->at(3 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Float40:
+		{
+			auto* _Pack = new FloatPack40();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+			_Pack->Data[2] = Input->at(2 + PackOffset);
+			_Pack->Data[3] = Input->at(3 + PackOffset);
+			_Pack->Data[4] = Input->at(4 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Float48:
+		{
+			auto* _Pack = new FloatPack48();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+			_Pack->Data[2] = Input->at(2 + PackOffset);
+			_Pack->Data[3] = Input->at(3 + PackOffset);
+			_Pack->Data[4] = Input->at(4 + PackOffset);
+			_Pack->Data[5] = Input->at(5 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Float56:
+		{
+			auto* _Pack = new FloatPack56();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+			_Pack->Data[2] = Input->at(2 + PackOffset);
+			_Pack->Data[3] = Input->at(3 + PackOffset);
+			_Pack->Data[4] = Input->at(4 + PackOffset);
+			_Pack->Data[5] = Input->at(5 + PackOffset);
+			_Pack->Data[6] = Input->at(6 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		case PackType::PT_Float64:
+		{
+			auto* _Pack = new FloatPack64();
+
+			_Pack->Data[0] = Input->at(0 + PackOffset);
+			_Pack->Data[1] = Input->at(1 + PackOffset);
+			_Pack->Data[2] = Input->at(2 + PackOffset);
+			_Pack->Data[3] = Input->at(3 + PackOffset);
+			_Pack->Data[4] = Input->at(4 + PackOffset);
+			_Pack->Data[5] = Input->at(5 + PackOffset);
+			_Pack->Data[6] = Input->at(6 + PackOffset);
+			_Pack->Data[7] = Input->at(7 + PackOffset);
+
+			Output.Packs.push_back(_Pack);
+
+			PackOffset += _Pack->Size;
+		}
+		}
+	}
 
 	return Output;
 }
